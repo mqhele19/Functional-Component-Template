9bd8e2fe09318ab50dabe33e6810d25bd3b2b2ac
MqheleMugqakayiseMguni <mqhelemguni19@gmail.com>
2023-06-12 04:12:26 +0200

    Add files via upload

---
+++ Functional Component Template.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const = () => {
+	return (
+	<View>
+	<Text>Just a component</Text>
+	</View>
+	);
+	};
+	
+	export default ;
